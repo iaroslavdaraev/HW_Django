236516eef624d9d5805124b805e81757976b3c47
Iaroslav <yaroslav.da@mail.ru>
2024-03-07 07:53:40 +0300

    fix task 2

--- main.py
+++ main.py
@@ -305,11 +305,11 @@ class MyServer(BaseHTTPRequestHandler):
                     <form>
                         <div class="mb-4">
                             <label for="exampleInputText" class="form-label">Имя</label>
-                            <input type="text" class="form-control" id="exampleInputText">
+                            <input name="name" type="text" class="form-control" id="exampleInputText">
                         </div>
                         <div class="mb-4">
-                            <label for="exampleInputEmail" class="form-label">Email</label>
-                            <input type="password" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp">
+                            <label for="exampleInputEmail1" class="form-label">Email</label>
+                            <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                         </div>
                         <button type="submit" class="btn btn-primary">Отправить</button>
